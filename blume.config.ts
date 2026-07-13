a484626cc7e0312e3590fc766683a7f37c49f3d2
Edu Wass <eduwass@gmail.com>
2026-07-13 20:14:36 +0200

    docs: github repo link in header

--- blume.config.ts
+++ blume.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'blume'
 export default defineConfig({
   title: 'edge-language-tools',
   description: 'Type safety and editor tooling for EdgeJS templates.',
+  github: {
+    owner: 'eduwass',
+    repo: 'edge-language-tools',
+    branch: 'main',
+  },
   deployment: {
     site: 'https://eduwass.github.io/edge-language-tools',
     base: '/edge-language-tools',
